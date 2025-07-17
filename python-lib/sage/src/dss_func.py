84b07d8593507f868e3b97235a397dcc6703bfb2
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-17 20:40:41 +0000

    Edited file 'python-lib/sage/src/dss_func.py'

--- python-lib/sage/src/dss_func.py
+++ python-lib/sage/src/dss_func.py
@@ -6,6 +6,24 @@ import pandas as pd
 import tomllib
 
 
+def collect_modules(module):
+    import streamlit as st
+    d = {}
+    directory = module.__path__[0]
+    for root, _, files in os.walk(directory):
+        for f in files:
+            if f.endswith(".py") and f != "__init__.py":
+                module_name = f[:-3]
+                path = root.replace(directory, "")
+                fp = os.path.join(root, f)
+                delimiters = r'[-_]'
+                words = re.split(delimiters, module_name)
+                capitalized_words = [word.capitalize() for word in words]
+                final_string = " ".join(capitalized_words)
+                d[final_string] = [module_name, fp]
+    return d
+
+
 def collect_display_data(dss_objs):
     display_data = []
     modules = collect_modules(dss_objs)
