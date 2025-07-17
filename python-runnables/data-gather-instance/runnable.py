6bcdc2aab6d66f88283bc7e8a986796664111831
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-17 20:54:25 +0000

    Edited file 'python-runnables/data-gather-instance/runnable.py'

--- python-runnables/data-gather-instance/runnable.py
+++ python-runnables/data-gather-instance/runnable.py
@@ -48,7 +48,7 @@ class MyRunnable(Runnable):
         # Load the INSIGHTS information
         categories_modules = dss_funcs.collect_modules(dss_objs)
 
-        return categories_modules.keys()
+        return "IS GTHIS WORKING"
     
     
     
