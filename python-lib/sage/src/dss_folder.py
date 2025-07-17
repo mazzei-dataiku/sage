5da550f34a7918bebbb0b932309944ba7080e82a
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-17 21:50:05 +0000

    Edited file 'python-lib/sage/src/dss_folder.py'

--- python-lib/sage/src/dss_folder.py
+++ python-lib/sage/src/dss_folder.py
@@ -1,5 +1,5 @@
 # ---------- DATAIKU REMOTE FOLDERS ----------------------------
-def write_remote_folder(self, client, df):
+def write_remote_folder_output(self, client, path, df):
     projet_handle = client.get_project(project_key=self.sage_project_key)
     fid = None
     for f in projet_handle.list_managed_folders():
@@ -9,5 +9,5 @@ def write_remote_folder(self, client, df):
     if not fid:
         raise Exception()
     folder = projet_handle.get_managed_folder(odb_id=fid)
-    r = folder.put_file("/testing_again.csv", df.to_csv(index=None))
+    r = folder.put_file(path, df.to_csv(index=None))
     return
