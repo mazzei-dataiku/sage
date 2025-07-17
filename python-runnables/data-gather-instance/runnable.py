ffdca67e3abb0b48dcd18e91236fbb9a6dd0335f
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-17 18:56:16 +0000

    Edited file 'python-runnables/data-gather-instance/runnable.py'

--- python-runnables/data-gather-instance/runnable.py
+++ python-runnables/data-gather-instance/runnable.py
@@ -4,13 +4,14 @@ class MyRunnable(Runnable):
     def __init__(self, project_key, config, plugin_config):
         self.project_key = project_key
         self.config = config
-        self.plugin_config = plugin_config
+        self.plugin_config = plugin_config # sage_project_key | sage_project_url | sage_project_api
         
     def get_progress_target(self):
         return None
 
     def run(self, progress_callback):
         a = []
+        #for k in self.plugin_config.keys():
         for k in self.plugin_config.keys():
             a.append(k)
         return " | ".join(a)
