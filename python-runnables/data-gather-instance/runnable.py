badca5cad77e54bc3bb9a1ee3fc3466f4e3d3c32
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-17 20:41:56 +0000

    Edited file 'python-runnables/data-gather-instance/runnable.py'

--- python-runnables/data-gather-instance/runnable.py
+++ python-runnables/data-gather-instance/runnable.py
@@ -13,22 +13,6 @@ except:
 #import sys
 #sys.path.append('/path/to/your/library/directory')
 
-def collect_modules(module):
-    d = {}
-    directory = module.__path__[0]
-    for root, _, files in os.walk(directory):
-        for f in files:
-            if f.endswith(".py") and f != "__init__.py":
-                module_name = f[:-3]
-                path = root.replace(directory, "")
-                fp = os.path.join(root, f)
-                delimiters = r'[-_]'
-                words = re.split(delimiters, module_name)
-                capitalized_words = [word.capitalize() for word in words]
-                final_string = " ".join(capitalized_words)
-                d[final_string] = [module_name, fp]
-    return d
-
 
 def run_modules(client, instance_name):
 
