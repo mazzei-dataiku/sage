5425f16b9848ab30212e7bf4acf72645ff997db0
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 14:49:19 +0000

    Edited file 'python-runnables/data-gather-audit/runnable.py'

--- python-runnables/data-gather-audit/runnable.py
+++ python-runnables/data-gather-audit/runnable.py
@@ -1,30 +1,43 @@
-# This file is the actual code for the Python runnable data-gather-audit
+try:
+    from sage.base_data import client_handle as dss_objs
+except:
+    dss_objs = False
+from sage.src import dss_funcs
+
+import pandas as pd
+from datetime import datetime
+
 from dataiku.runnables import Runnable
+from dataiku.customrecipe import get_recipe_config
 
+
+# Run Macro
 class MyRunnable(Runnable):
-    """The base interface for a Python runnable"""
-
     def __init__(self, project_key, config, plugin_config):
-        """
-        :param project_key: the project in which the runnable executes
-        :param config: the dict of the configuration of the object
-        :param plugin_config: contains the plugin settings
-        """
         self.project_key = project_key
         self.config = config
         self.plugin_config = plugin_config
+        self.sage_project_key = plugin_config.get("sage_project_key", None)
+        self.sage_project_url = plugin_config.get("sage_project_url", None)
+        self.sage_project_api = plugin_config.get("sage_project_api", None)
+        self.dt = datetime.utcnow()
         
     def get_progress_target(self):
-        """
-        If the runnable will return some progress info, have this function return a tuple of 
-        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
-        """
         return None
 
     def run(self, progress_callback):
-        """
-        Do stuff here. Can return a string or raise an exception.
-        The progress_callback is a function expecting 1 value: current progress
-        """
-        raise Exception("unimplemented")
+        # Test if modules are found
+        if not dss_objs:
+            raise Exception("No categories or modules found")
         
+        # Collect the modules && Run the modules
+        local_client = dss_funcs.build_local_client()
+        results = dss_funcs.run_modules(self, dss_objs, local_client)
+        
+        # return results
+        if results:
+            df = pd.DataFrame(results, columns=["instance_level", "path", "module_name", "step", "result", "message"])
+            del df["instance_level"]
+            html = df.to_html()
+            return html
+        raise Exception("FAILED TO RUN INSTANCE CHECKS")
