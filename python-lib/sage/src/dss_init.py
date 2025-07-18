bb165841e233fa134dedf4a96ad0db1065bc4b52
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 19:01:49 +0000

    Edited file 'python-lib/sage/src/dss_init.py'

--- python-lib/sage/src/dss_init.py
+++ python-lib/sage/src/dss_init.py
@@ -1,4 +1,25 @@
 
+t = """
+[default]
+trigger = '{"type": "temporal", "name": "Time-based", "delay": 5, "active": true, "params": {"repeatFrequency": 1, "frequency": "Daily", "startingFrom": "2025-07-16", "daysOfWeek": ["Wednesday"], "monthlyRunOn": "ON_THE_DAY", "minute": 0, "hour": 1, "timezone": "Canada/Eastern"}}'
+step = '{"type": "runnable", "name": "run_macro", "enabled": true, "alwaysShowComment": false, "runConditionType": "RUN_IF_STATUS_MATCH", "runConditionStatuses": ["SUCCESS", "WARNING"], "runConditionExpression": "", "resetScenarioStatus": false, "delayBetweenRetries": 10, "maxRetriesOnFail": 0, "params": {"runnableType": "REPLACE_MACRO_HERE",   "config": {}, "adminConfig": {}, "proceedOnFailure": false}}'
+
+[data_gather_instance]
+macro = "pyrunnable_sage_data-gather-instance"
+
+[data_gather_project]
+macro = "pyrunnable_sage_data-gather-project"
+
+[data_gather_audit]
+macro = "pyrunnable_sage_data-gather-audit"
+
+[data_gather_diskspace]
+macro = "pyrunnable_sage_data-gather-diskspace"
+
+[data_gather_filesystem]
+macro = "pyrunnable_sage_data-gather-filesystem"
+"""
+
 
 def create_worker(client):
     if "SAGE_WORKER" not in client.list_project_keys():
