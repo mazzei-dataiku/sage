ae2e7903eb1131a04cef7f70acce0d3617272d4d
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 19:05:23 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -8,6 +8,7 @@ class MyRunnable(Runnable):
         self.sage_project_key = plugin_config.get("sage_project_key", None)
         self.sage_project_url = plugin_config.get("sage_project_url", None)
         self.sage_project_api = plugin_config.get("sage_project_api", None)
+        self.sage_worker_key  = plugin_config.get("sage_worker_key", None)
         
     def get_progress_target(self):
         return None
