8fae9e1543b27d42a17dd027343ff4c6309a4b54
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 19:00:55 +0000

    Edited file 'python-lib/sage/src/dss_init.py'

--- python-lib/sage/src/dss_init.py
+++ python-lib/sage/src/dss_init.py
@@ -6,3 +6,37 @@ def create_worker(client):
     else:
         project_handle = client.get_project(project_key="SAGE_WORKER")
     return project_handle
+
+
+def get_dss_commits(project_handle):
+    dataset = project_handle.get_dataset("dss_commits")
+    if not dataset.exists():
+        dataset = project_handle.create_dataset(
+            dataset_name = "dss_commits",
+            type = "StatsDB",
+            params = {
+                'view': 'COMMITS',
+                'orderByDate': False,
+                'clusterTasks': {},
+                'commits': {},
+                'jobs': {},
+                'scenarioRuns': {},
+                'flowActions': {}
+            }
+        )
+        schema = {
+            "columns": [
+                {"name": "project_key", "type": "string"},
+                {"name": "commit_id", "type": "string"},
+                {"name": "author", "type": "string"},
+                {"name": "timestamp", "type": "bigint"},
+                {"name": "added_files", "type": "int"},
+                {"name": "added_lines", "type": "int"},
+                {"name": "removed_files", "type": "int"},
+                {"name": "removed_lines", "type": "int"},
+                {"name": "changed_files", "type": "int"},
+            ],
+            "userModified": True,
+        }
+        r = dataset.set_schema(schema=schema)
+    return
