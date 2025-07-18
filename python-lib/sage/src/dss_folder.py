dcdc08e99c498c3c68b2069e4a0cb21db85029fa
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 21:08:15 +0000

    Edited file 'python-lib/sage/src/dss_folder.py'

--- python-lib/sage/src/dss_folder.py
+++ python-lib/sage/src/dss_folder.py
@@ -12,4 +12,42 @@ def write_remote_folder_output(self, client, path, df):
     r = folder.put_file(path, df.to_csv(index=None))
     return
 
+
 # ---------- DATAIKU LOCAL FOLDERS -----------------------------
+def get_folder(folder_name):
+    folder = dataiku.Folder(
+        lookup = folder_name,
+        project_key = dataiku.default_project_key(),
+        ignore_flow = True
+        )
+    try:
+        folder.get_id()
+    except:
+        folder = create_folder(folder_name)
+    return folder
+
+
+def create_folder(folder_name):
+    client = dataiku.api_client()
+    project = client.get_default_project()    
+    # Create Folder
+    folder_handle = project.create_managed_folder(
+        name = folder_name,
+        connection_name = "filesystem_folders"
+    )
+    if folder_name == "partitioned_data":
+        settings = folder_handle.get_settings()
+        settings.remove_partitioning()
+        settings.add_discrete_partitioning_dimension("instance_name")
+        settings.add_discrete_partitioning_dimension("category")
+        settings.add_discrete_partitioning_dimension("module")
+        settings.add_time_partitioning_dimension("date", period='DAY')
+        settings.set_partitioning_file_pattern("%{instance_name}/%{category}/%{module}/%Y/%M/%D/.*")
+        settings.save()
+    # Return Folder object
+    folder = dataiku.Folder(
+        lookup = folder_name,
+        ignore_flow = True,
+        project_key = dataiku.default_project_key()
+    )
+    return folder
