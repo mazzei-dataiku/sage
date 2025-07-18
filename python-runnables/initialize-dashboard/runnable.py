20055cead88dad9797b5b59d561ee2c483228cd4
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 19:58:50 +0000

    Edited file 'python-runnables/initialize-dashboard/runnable.py'

--- python-runnables/initialize-dashboard/runnable.py
+++ python-runnables/initialize-dashboard/runnable.py
@@ -10,4 +10,12 @@ class MyRunnable(Runnable):
         return None
 
     def run(self, progress_callback):
-        raise Exception("unimplemented")
+        # Get local client and name
+        local_client = dss_funcs.build_local_client()
+        instance_name = dss_funcs.get_dss_name(local_client)
+        
+        # change directory and get audit logs
+        root_path = local_client.get_instance_info().raw["dataDirPath"]
+        os.chdir(root_path)
+        
+    return
