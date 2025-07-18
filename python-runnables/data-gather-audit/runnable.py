ad7191104248e02a8e5fb29fd7c09852e8a1b4c6
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 16:20:30 +0000

    Edited file 'python-runnables/data-gather-audit/runnable.py'

--- python-runnables/data-gather-audit/runnable.py
+++ python-runnables/data-gather-audit/runnable.py
@@ -59,7 +59,7 @@ class MyRunnable(Runnable):
         for topic in df.topic.unique():
             try:
                 write_path = f"/{instance_name}/audit/{topic}/{dt_year}/{dt_month}/{dt_day}/data.csv"
-                dss_folder.write_remote_folder_output(self, remote_client, write_path, df)
+                #dss_folder.write_remote_folder_output(self, remote_client, write_path, df)
                 results.append(["write/save", False, None])
             except Exception as e:
                 results.append(["write/save", True, e])
