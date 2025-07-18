34add757190ae216c1eabf18f84b049d01032826
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 00:32:10 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -11,6 +11,7 @@ class MyRunnable(Runnable):
         return None
 
     def run(self, progress_callback):
+        # {'$$hashKey': 'object:6605', 'worker_url': 'https://mazzei-designer.fe-aws.dkucloud-dev.com', 'worker_api': 'dkuaps-MSleUwXxL2lCXEMQZVwiJNjMHfS0zIJ2'}
         for api_config in self.plugin_config["api_configs"]:
             logging.error(api_config)
             
