9e9bbdc486e0a510a27358887749b87a2cd678f8
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-18 17:05:17 +0000

    Edited file 'python-runnables/data-gather-filesystem/runnable.py'

--- python-runnables/data-gather-filesystem/runnable.py
+++ python-runnables/data-gather-filesystem/runnable.py
@@ -21,4 +21,6 @@ class MyRunnable(Runnable):
         return None
 
     def run(self, progress_callback):
-        
+        # Get local client and name
+        local_client = dss_funcs.build_local_client()
+        instance_name = dss_funcs.get_dss_name(local_client)
