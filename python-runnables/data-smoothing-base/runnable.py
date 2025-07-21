8dae4bc41807f274b795a01e68e3e67143ae0888
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-21 20:33:02 +0000

    Edited file 'python-runnables/data-smoothing-base/runnable.py'

--- python-runnables/data-smoothing-base/runnable.py
+++ python-runnables/data-smoothing-base/runnable.py
@@ -14,5 +14,13 @@ class MyRunnable(Runnable):
         return None
 
     def run(self, progress_callback):
-        raise Exception("unimplemented")
+        # get partitioned folder
+        local_client = build_local_client() # dss_funcs
+        project_handle = local_client.get_project(project_key="SAGE_DASHBOARD") # self.sage_project_key
+        folder = get_folder("SAGE_DASHBOARD", project_handle, "partitioned_data") # dss_folder, self.sage_project_key
         
+        # list partitions and turn into a df
+        partitions = folder.list_partitions()
+        folder_df = pd.DataFrame(partitions, columns=["partitions"])
+        cols = ["instance_name", "category", "module", "dt"]
+        folder_df[cols] = folder_df["partitions"].str.split("|", expand=True)
