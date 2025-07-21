d1f15d1865fe436e487baa756cecc976bcad4563
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-21 20:23:53 +0000

    Edited file 'python-lib/sage/src/dss_folder.py'

--- python-lib/sage/src/dss_folder.py
+++ python-lib/sage/src/dss_folder.py
@@ -1,4 +1,8 @@
+# plugin: src.dss_folder
 import dataiku
+import pandas as pd
+import json
+import warnings
 
 
 # ---------- DATAIKU LOCAL FOLDERS -----------------------------
@@ -12,7 +16,7 @@ def get_folder(sage_project_key, project_handle, folder_name):
         folder.get_id()
     except:
         folder = create_folder(project_handle, folder_name)
-    return
+    return folder
 
 
 def create_folder(project_handle, folder_name):
@@ -30,6 +34,40 @@ def create_folder(project_handle, folder_name):
         settings.set_partitioning_file_pattern("%{instance_name}/%{category}/%{module}/%Y/%M/%D/.*")
         settings.save()
     return
+
+
+def write_local_folder_output(sage_project_key, project_handle, folder_name, path, data, data_type="DF"):
+    folder = get_folder(sage_project_key, project_handle, folder_name)
+    if data_type == "DF":
+        with folder.get_writer(path) as w:
+            w.write(data.to_csv(index=False).encode("utf-8"))
+    elif data_type == "JSON":
+        with folder.get_writer(path) as w:
+            w.write(str.encode(json.dumps(data, indent=4)))
+    return
+
+
+def read_local_folder_input(sage_project_key, project_handle, folder_name, path, data_type="DF"):
+    folder = get_folder(sage_project_key, project_handle, folder_name)
+    if data_type == "DF":
+        with folder.get_download_stream(path) as reader:
+            data = pd.read_csv(reader)
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")  # Ignore all warnings within this block
+                data = function_with_warning(data)
+    elif data_type == "JSON":
+        with folder.get_download_stream(path) as reader:
+            data = json.loads(reader.data)
+    return data
+
+
+def function_with_warning(df):
+    for c in df.columns:
+        if df[c].dtype == "object":
+            temp_col = pd.to_datetime(df[c],  errors='coerce')
+            if temp_col.notna().all():
+                df[c] = temp_col
+    return df
 
 
 # ---------- DATAIKU REMOTE FOLDERS ----------------------------
