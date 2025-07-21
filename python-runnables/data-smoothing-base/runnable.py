2ad2a09e95cc515904b4b4f0906b74d8cbbdb774
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-21 20:33:24 +0000

    Edited file 'python-runnables/data-smoothing-base/runnable.py'

--- python-runnables/data-smoothing-base/runnable.py
+++ python-runnables/data-smoothing-base/runnable.py
@@ -24,3 +24,43 @@ class MyRunnable(Runnable):
         folder_df = pd.DataFrame(partitions, columns=["partitions"])
         cols = ["instance_name", "category", "module", "dt"]
         folder_df[cols] = folder_df["partitions"].str.split("|", expand=True)
+        
+        # get latest partition
+        max_date = folder_df['dt'].max()
+        write_local_folder_output( # dss_folder
+            sage_project_key = "SAGE_DASHBOARD",
+            project_handle = project_handle,
+            folder_name = "base_data",
+            path = f"/partition.csv",
+            data_type = "DF",
+            data = pd.DataFrame([max_date], columns=["latest_partition"])
+        )
+        filtered_df = folder_df[folder_df['dt'] == max_date]
+        
+        # Loop over the sets and gather
+groups = filtered_df.groupby(by=["category", "module"])
+for i, g in groups:
+    category, module = i
+    # loop over and build consolidated df
+    df = pd.DataFrame()
+    for partition in g["partitions"].tolist():
+        paths = folder.list_paths_in_partition(partition=partition)
+        for path in paths:
+            tdf = read_local_folder_input( # dss_folder
+                "SAGE_DASHBOARD", project_handle, "partitioned_data", path
+            )
+            if df.empty:
+                df = tdf
+            else:
+                df = pd.concat([df, tdf], ignore_index=True)
+        # Write consolidated DF to folder
+        write_local_folder_output( # dss_folder
+            sage_project_key = "SAGE_DASHBOARD",
+            project_handle = project_handle,
+            folder_name = "base_data",
+            path = f"/{category}/{module}.csv",
+            data_type = "DF",
+            data = df
+        )
+        
+        return
