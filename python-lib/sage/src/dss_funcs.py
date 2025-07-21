910942be7b87d6d4cb0e6a4e861291508ce30a53
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-21 20:24:09 +0000

    Edited file 'python-lib/sage/src/dss_funcs.py'

--- python-lib/sage/src/dss_funcs.py
+++ python-lib/sage/src/dss_funcs.py
@@ -1,3 +1,4 @@
+# plugin: src.dss_funcs
 import dataiku
 import dataikuapi
 import os
@@ -74,36 +75,3 @@ def get_nested_value(data, keys):
         else:
             return False
     return current
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def collect_modules(dss_objs):
-    modules = {}
-    directory = dss_objs.__path__[0]
-    for root, _, files in os.walk(directory):
-        for f in files:
-            if not f.endswith(".py") or f == "__init__.py":
-                continue
-            module_name = f[:-3]
-            path = root.replace(directory, "")
-            fp = os.path.join(root, f)
-            l = [path, fp]
-            if module_name in modules.keys():
-                modules[module_name].append(l)
-            else:
-                modules[module_name] = []
-                modules[module_name].append(l)
-    return modules
