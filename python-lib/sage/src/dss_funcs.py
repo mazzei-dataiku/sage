a70b96beca4e8e2b4e2cf8dda38efffc5024014e
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-22 20:29:10 +0000

    Edited file 'python-lib/sage/src/dss_funcs.py'

--- python-lib/sage/src/dss_funcs.py
+++ python-lib/sage/src/dss_funcs.py
@@ -57,7 +57,7 @@ def run_modules(self, dss_objs, handle, project_key = None):
             if "instance_name" not in df.columns:
                 df["instance_name"] = instance_name
             try:
-                remote_client = build_remote_client(self.sage_project_url, self.sage_project_api)
+                remote_client = build_remote_client(self.sage_project_url, self.sage_project_api, self.)
                 dt_year  = str(self.dt.year)
                 dt_month = str(f'{self.dt.month:02d}')
                 dt_day   = str(f'{self.dt.day:02d}')
