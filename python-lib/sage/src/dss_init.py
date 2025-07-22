50ce3f6576e68af253035b25d6434cfa0ecb576c
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-22 20:51:37 +0000

    Edited file 'python-lib/sage/src/dss_init.py'

--- python-lib/sage/src/dss_init.py
+++ python-lib/sage/src/dss_init.py
@@ -25,11 +25,11 @@ macro = "pyrunnable_sage_data-gather-filesystem"
 
 dashboard_scenarios = """
 [default]
-trigger = '{"type": "temporal", "name": "Time-based", "delay": 5, "active": true, "params": {"repeatFrequency": 1, "frequency": "Daily", "startingFrom": "2025-07-16", "daysOfWeek": ["Wednesday"], "monthlyRunOn": "ON_THE_DAY", "minute": 0, "hour": 17, "timezone": "Canada/Eastern"}}'
+trigger = '{"type": "temporal", "name": "Time-based", "delay": 5, "active": true, "params": {"repeatFrequency": 1, "frequency": "Daily", "startingFrom": "2025-07-16", "daysOfWeek": ["Wednesday"], "monthlyRunOn": "ON_THE_DAY", "minute": 0, "hour": 18, "timezone": "Canada/Eastern"}}'
 step = '{"type": "runnable", "name": "run_macro", "enabled": true, "alwaysShowComment": false, "runConditionType": "RUN_IF_STATUS_MATCH", "runConditionStatuses": ["SUCCESS", "WARNING"], "runConditionExpression": "", "resetScenarioStatus": false, "delayBetweenRetries": 10, "maxRetriesOnFail": 0, "params": {"runnableType": "REPLACE_MACRO_HERE",   "config": {}, "adminConfig": {}, "proceedOnFailure": false}}'
 
 [refresh_base_data]
-macro = "pyrunnable_sage_data-gather-instance"
+macro = "pyrunnable_sage_data-smoothing-base"
 
 """
 
