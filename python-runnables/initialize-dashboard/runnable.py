756fc4c630ff19110e3605a58b03f54efdc5f08d
smazzei <Stephen.Mazzei@dataiku.com>
2025-07-22 20:58:06 +0000

    Edited file 'python-runnables/initialize-dashboard/runnable.py'

--- python-runnables/initialize-dashboard/runnable.py
+++ python-runnables/initialize-dashboard/runnable.py
@@ -78,6 +78,7 @@ class MyRunnable(Runnable):
                 library = project_handle.get_library()
                 file = library.add_file("python/sage/init.txt")
                 file.delete()
+                
                 file = library.get_file("python/sage/src/dss_init.py")
                 file.delete()
                 results.append(["Library Refresh", True, None])
